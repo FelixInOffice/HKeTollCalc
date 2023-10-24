7de133d353e7772f7591469022da932d750c41eb
Felix Tuen <felix.tuen@zlashy.com>
2023-10-24 10:52:08 +0800

    fix: try fixing route  problem

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Record from '../views/RecordView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  mode: 'hash',
   routes: [
     {
       path: '/',

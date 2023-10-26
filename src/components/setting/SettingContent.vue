80bba0c638f75d2d7dd5e8be45802a7a3ef8f316
Felix Tuen <felix.tuen@zlashy.com>
2023-10-26 17:14:41 +0800

    chore: simplify the code

--- src/components/setting/SettingContent.vue
+++ src/components/setting/SettingContent.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="setting__wrapper">
-    <div class="setting__wrapper--item">
-      <div class="item--name">顯示速度計</div>
-      <ToggleSwitch :status="settingStore.showSpeedMeter" @toggleSwitch="toggleShowSpeedMeter()" />
-    </div>
-
-    <div class="setting__wrapper--item">
-      <div class="item--name">顯示 GPS 狀態</div>
-      <ToggleSwitch :status="settingStore.showGPSStatus" @toggleSwitch="toggleShowGPSStatus()" />
+    <div
+      v-for="settingItem in settingData.content"
+      class="setting__wrapper--item"
+    >
+      <div class="item--name">{{ settingItem.name }}</div>
+      <ToggleSwitch
+        :status="settingStore.setting[settingItem.key]"
+        @toggleSwitch="toggleSetting(settingItem)"
+      />
     </div>
   </div>
 </template>
@@ -15,26 +16,17 @@
 <script setup>
 import ToggleSwitch from "../common/ToggleSwitch.vue";
 import { useSettingStore } from "@/stores/setting";
+import settingData from "@/pageData/settingData";
 
 const settingStore = useSettingStore();
 
-function toggleShowSpeedMeter() {
-  if (settingStore.showSpeedMeter) {
-    localStorage.setItem("showSpeedMeter", false);
-    settingStore.showSpeedMeter = false;
+function toggleSetting(settingItem) {
+  if (settingStore.setting[settingItem.key]) {
+    localStorage.setItem(settingItem.key, false);
+    settingStore.setting[settingItem.key] = false;
   } else {
-    localStorage.setItem("showSpeedMeter", true);
-    settingStore.showSpeedMeter = true;
-  }
-}
-
-function toggleShowGPSStatus() {
-  if (settingStore.showGPSStatus) {
-    localStorage.setItem("showGPSStatus", false);
-    settingStore.showGPSStatus = false;
-  } else {
-    localStorage.setItem("showGPSStatus", true);
-    settingStore.showGPSStatus = true;
+    localStorage.setItem(settingItem.key, true);
+    settingStore.setting[settingItem.key] = true;
   }
 }
 </script>

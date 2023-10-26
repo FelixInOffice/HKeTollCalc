c52a269e111a409accac4c8680db9ca5a1db425d
Felix Tuen <felix.tuen@zlashy.com>
2023-10-26 18:05:26 +0800

    feat(record): add delete confirm

--- src/components/record/Records.vue
+++ src/components/record/Records.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="recordArray__wrapper">
     <div class="recordArray__wrapper--topBar">
-      Total: {{ tunnelFeeTotal }}
+      <div class="recordArray__wrapper--topBar--total">
+        合計：HKD {{ tunnelFeeTotal }}
+      </div>
+      <div class="recordArray__wrapper--topBar--deleteAll">
+        <button
+          :disabled="tunnelRecordArray.length === 0"
+          @click="confirmDeleteAllPopupControl = !confirmDeleteAllPopupControl"
+        >
+          刪除全部
+        </button>
+      </div>
     </div>
     <div
       class="recordArray__wrapper--record"
@@ -17,16 +27,31 @@
           {{ record.time }}
         </div>
       </div>
-      <button @click="deleteRecord(idx)">Delete</button>
+      <button @click="deleteRecord(idx)">刪除</button>
     </div>
+
+    <Popup
+      v-if="confirmDeleteAllPopupControl"
+      title="確定刪除全部？"
+      content="此動作將會刪除所有紀錄，並無法復原。"
+    >
+      <button @click="deleteAllRecord()">確定</button>
+      <button
+        @click="confirmDeleteAllPopupControl = !confirmDeleteAllPopupControl"
+      >
+        取消
+      </button>
+    </Popup>
   </div>
 </template>
 
 <script setup>
 import { ref, watch, onMounted } from "vue";
+import Popup from "../common/Popup.vue";
 
 const tunnelRecordArray = ref(getTunnelRecord());
 const tunnelFeeTotal = ref(0);
+const confirmDeleteAllPopupControl = ref(false);
 
 onMounted(() => {
   tunnelFeeTotal.value = tunnelRecordArray.value.reduce((acc, curr) => {
@@ -51,9 +76,16 @@ function getTunnelRecord() {
 }
 
 function deleteRecord(index) {
-  const storedArray = JSON.parse(localStorage.getItem("tunnelRecordArray")) || [];
+  const storedArray =
+    JSON.parse(localStorage.getItem("tunnelRecordArray")) || [];
   storedArray.splice(index, 1);
   localStorage.setItem("tunnelRecordArray", JSON.stringify(storedArray));
   tunnelRecordArray.value = storedArray;
+}
+
+function deleteAllRecord() {
+  confirmDeleteAllPopupControl.value = !confirmDeleteAllPopupControl.value;
+  localStorage.removeItem("tunnelRecordArray");
+  tunnelRecordArray.value = [];
 }
 </script>
